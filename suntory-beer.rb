a713992882fcb8ae80f0e52205e75b5ce6ab5b6a
gecko655 <aqwsedrft1234@yahoo.co.jp>
2016-06-09 22:02:15 +0900

    MASTER'S DREAM

--- suntory-beer.rb
+++ suntory-beer.rb
@@ -17,15 +17,19 @@ driver = Selenium::WebDriver.for :remote, :url => "http://"+hostname+":4444/wd/h
 begin
   driver.manage.timeouts.implicit_wait = 10
 
-  logger.info("Accessing to https://premiumcp-coupon.jp/kaoruale/attestation.html")
-  driver.navigate.to "https://premiumcp-coupon.jp/kaoruale/attestation.html"
+  logger.info("Accessing to https://premiumcp-coupon.jp/mastersdream/attestation.html")
+  driver.navigate.to "https://premiumcp-coupon.jp/mastersdream/attestation.html"
 
   logger.info("age check")
-  twenty_button = driver.find_element(:class => "ade")
+  twenty_button = driver.find_element(:class => "fade")
+  twenty_button.click
+
+  logger.info("agreement check")
+  twenty_button = driver.find_element(:id => "agreement")
   twenty_button.click
 
   logger.info("Select twitter to apply")
-  apply_with_twitter = driver.find_element(:class => "fade")
+  apply_with_twitter = driver.find_elements(:class => "fade")[0]
   apply_with_twitter.click
 
   logger.info("Twitter authentication")
@@ -35,15 +39,15 @@ begin
 
   logger.info("Input apply comment")
   driver.find_element(:id => "text_length").send_keys apply_comment
-  driver.find_element(:class => "fade").find_element(:tag_name => "button").click
+  driver.find_element(:class => "fade").click
 
   logger.info("Confirm")
-  driver.find_elements(:class => "fade")[1].find_element(:tag_name => "button").click
+  driver.find_elements(:class => "fade")[1].click
 
   sleep 5
 
-  result_string = driver.find_element(:id => "content")
-      .find_elements(:tag_name => "img")[1].attribute("alt")
+  result_string = driver.find_element(:class => "lottery")
+      .find_elements(:tag_name => "img").attribute("src")
   ResultNotify.notify(result_string,twitterID)
   logger.info(result_string)
 
