86e661c3953fd4dea7c4fc4238b605e8c3d3108d
gecko655 <aqwsedrft1234@yahoo.co.jp>
2017-02-03 00:23:58 +0900

    Implemented for http://kokushiboricp-coupon.jp/

--- suntory-beer.rb
+++ suntory-beer.rb
@@ -17,19 +17,20 @@ driver = Selenium::WebDriver.for :remote, :url => "http://"+hostname+":4444/wd/h
 begin
   driver.manage.timeouts.implicit_wait = 10
 
-  logger.info("Accessing to http://s.196cp.jp/")
-  driver.navigate.to "http://s.196cp.jp/"
+  logger.info("Accessing to http://kokushiboricp-coupon.jp/")
+  driver.navigate.to "http://kokushiboricp-coupon.jp/"
 
   logger.info("age check")
   twenty_button = driver.find_element(:id => "ac_modal_btn_yes")
   twenty_button.click
 
   logger.info("agree")
-  twenty_button = driver.find_element(:id => "agreement")
+  twenty_button = driver.find_element(:class => "icheckbox_minimal-aero")
   twenty_button.click
 
   logger.info("Select twitter to apply")
-  apply_with_twitter = driver.find_elements(:class => "fade")[0]
+  apply_with_twitter = driver.find_element(:class => "btns-social")
+    .find_element(:class => "fade")
   apply_with_twitter.click
 
   logger.info("Twitter authentication")
@@ -39,15 +40,15 @@ begin
   sleep 3
 
   logger.info("Input apply comment")
-  driver.find_element(:id => "tweetText").send_keys apply_comment
+  driver.find_element(:name => "twitter_comment").send_keys apply_comment
   driver.find_elements(:tag_name => "button")[0].click
 
   logger.info("Confirm")
-  driver.find_element(:class => "btnbox").find_elements(:tag_name => "img")[1].click
+  driver.find_element(:class => "btns").find_elements(:tag_name => "img")[1].click
 
   sleep 10
 
-  result_string = driver.find_element(:id => "content")
+  result_string = driver.find_element(:class => "lottery")
       .find_elements(:tag_name => "img")[0].attribute("src")
   logger.info(result_string)
   ResultNotify.notify(result_string,twitterID)
